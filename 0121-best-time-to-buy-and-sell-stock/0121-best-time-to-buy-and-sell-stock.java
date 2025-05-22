fbc449674a99d3c788f2abc127ad2afa38621e34
Y Charan Kumar Naidu <156291108+Yaddalapalli-Charan-Kumar-Naidu@users.noreply.github.com>
2025-05-22 18:03:31 +0530

    Time: 3 ms (7.09%), Space: 61.5 MB (74.38%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,28 +1,36 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int sp[]=new int[prices.length];
-        int bp[]=new int[prices.length];
-        sp[prices.length-1]=prices[prices.length-1];
-        bp[0]=prices[0];
-        for(int i=0;i<prices.length;i++){
-            if(i==0){
-                continue;
-            }
-            else{
-                 bp[i]=Math.min(prices[i],bp[i-1]);
-            }
-            if(i==prices.length-1){
-                continue;
-            }
-            else{
-                sp[prices.length-i-1]=Math.max(prices[prices.length-i-1],sp[i+1]);
-            }           
-        }
+        // int sp[]=new int[prices.length];
+        // int bp[]=new int[prices.length];
+        // sp[prices.length-1]=prices[prices.length-1];
+        // bp[0]=prices[0];
+        // for(int i=0;i<prices.length;i++){
+        //     if(i==0){
+        //         continue;
+        //     }
+        //     else{
+        //          bp[i]=Math.min(prices[i],bp[i-1]);
+        //     }
+        //     if(i==prices.length-1){
+        //         continue;
+        //     }
+        //     else{
+        //         sp[prices.length-i-1]=Math.max(prices[prices.length-i-1],sp[i+1]);
+        //     }           
+        
+        // int maxReturn=0;
+        // for(int i=0;i<prices.length;i++){
+        //     if(sp[i]>bp[i]){
+        //         maxReturn=Math.max(maxReturn,sp[i]-bp[i]);
+        //     }
+        // }
+        // return maxReturn;
+        
+        int bp=prices[0];
         int maxReturn=0;
-        for(int i=0;i<prices.length;i++){
-            if(sp[i]>bp[i]){
-                maxReturn=Math.max(maxReturn,sp[i]-bp[i]);
-            }
+        for(int i=1;i<prices.length;i++){
+            maxReturn=Math.max(prices[i]-bp,maxReturn);
+            bp=Math.min(prices[i],bp);
         }
         return maxReturn;
     }
