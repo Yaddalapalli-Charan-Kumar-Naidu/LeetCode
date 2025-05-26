2f6303a7745d60eb95674f5f8c065ee52f428974
Y Charan Kumar Naidu <156291108+Yaddalapalli-Charan-Kumar-Naidu@users.noreply.github.com>
2025-05-26 22:33:48 +0530

    Time: 0 ms (100%), Space: 42.2 MB (77.59%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,14 +1,36 @@
 class Solution {
+    
+    public void normalSwap(int[][] matrix,int i,int j){
+        matrix[i][j]^=matrix[j][i];
+        matrix[j][i]^=matrix[i][j];
+        matrix[i][j]^=matrix[j][i];
+
+    }
     public void rotate(int[][] matrix) {
-        int[][] temp=new int[matrix.length][matrix[0].length];
+        // int[][] temp=new int[matrix.length][matrix[0].length];
+        // for(int i=0;i<matrix.length;i++){
+        //     for(int j=0;j<matrix[0].length;j++){
+        //         temp[j][matrix[0].length-1-i]=matrix[i][j];
+        //     }
+        // }
+        // for(int i=0;i<matrix.length;i++){
+        //     for(int j=0;j<matrix[0].length;j++){
+        //         matrix[i][j]=temp[i][j];
+        //     }
+        // }
         for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                temp[j][matrix[0].length-1-i]=matrix[i][j];
+            for(int j=i+1;j<matrix[0].length;j++){
+                normalSwap(matrix,i,j);
             }
         }
         for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                matrix[i][j]=temp[i][j];
+            int left = 0, right = matrix.length- 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
             }
         }
     }
