75907cfbc010d62cc435a3ed076ac44192055f30
Y Charan Kumar Naidu <156291108+Yaddalapalli-Charan-Kumar-Naidu@users.noreply.github.com>
2025-08-10 20:14:47 +0530

    Time: 6 ms (73.41%), Space: 45.2 MB (15.04%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,20 +1,32 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if(s.length()==0){
-            return 0;
+        // if(s.length()==0){
+        //     return 0;
+        // }
+        // int maxLen=Integer.MIN_VALUE;
+        // for(int i=0;i<s.length();i++){
+        //     int localMax=0;
+        //     HashSet<Character> set=new HashSet<>();
+        //     int j=i;
+        //     while(j<s.length() && !set.contains(s.charAt(j))){
+        //         set.add(s.charAt(j));
+        //         j++;
+        //         localMax++;
+        //     }
+        //     maxLen=Math.max(maxLen,localMax);
+        // }        
+        // return maxLen;
+        
+       int left=0,maxLen=0;
+       HashSet<Character> set=new HashSet<>();
+       for(int right=0;right<s.length();right++){
+        while(set.contains(s.charAt(right))){
+            set.remove(s.charAt(left));
+            left++;
         }
-        int maxLen=Integer.MIN_VALUE;
-        for(int i=0;i<s.length();i++){
-            int localMax=0;
-            HashSet<Character> set=new HashSet<>();
-            int j=i;
-            while(j<s.length() && !set.contains(s.charAt(j))){
-                set.add(s.charAt(j));
-                j++;
-                localMax++;
-            }
-            maxLen=Math.max(maxLen,localMax);
-        }        
-        return maxLen;
+        set.add(s.charAt(right));
+        maxLen=Math.max(right-left+1,maxLen);
+       }
+       return maxLen;
     }
 }
