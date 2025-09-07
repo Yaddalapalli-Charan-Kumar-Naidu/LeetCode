c8257721d379dd0109e96a818cb0f150f8bb827b
Y Charan Kumar Naidu <156291108+Yaddalapalli-Charan-Kumar-Naidu@users.noreply.github.com>
2025-09-07 16:08:18 +0530

    Time: 1 ms (81.25%), Space: 40.6 MB (94.84%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,18 +1,34 @@
 class Solution {
     public int reverse(int x) {
-        boolean neg=false;
-        if(x<0){
-            neg=true;
-            x=-x;
+        // boolean neg=false;
+        // if(x<0){
+        //     neg=true;
+        //     x=-x;
+        // }
+        // StringBuilder str=new StringBuilder(Integer.toString(x));
+        // str.reverse();
+        // try{
+        //     int val=Integer.parseInt(str.toString());
+        //    return neg==true? -val: val;
+        // }
+        // catch(Exception e){
+        //     return 0;
+        // }
+
+        int res=0;
+        int temp=x;
+        while(temp!=0){
+            int d=temp%10;
+            if(res>Integer.MAX_VALUE/10 || res<Integer.MIN_VALUE/10){
+                return 0;
+            }
+            if((res==Integer.MAX_VALUE/10 && d>7) ||(res==Integer.MIN_VALUE/10 && d<-8)){
+                return 0;
+            }
+                        res=res*10+d;
+
+            temp=temp/10;
         }
-        StringBuilder str=new StringBuilder(Integer.toString(x));
-        str.reverse();
-        try{
-            int val=Integer.parseInt(str.toString());
-           return neg==true? -val: val;
-        }
-        catch(Exception e){
-            return 0;
-        }
+        return res;
     }
 }
