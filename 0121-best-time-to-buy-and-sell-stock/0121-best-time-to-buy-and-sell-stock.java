76f44c8ed008837485825dbb93f7d5bb2393dcbc
Y Charan Kumar Naidu <156291108+Yaddalapalli-Charan-Kumar-Naidu@users.noreply.github.com>
2025-09-09 11:58:22 +0530

    Time: 3 ms (5.53%), Space: 58.8 MB (97.68%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,37 +1,19 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        // int sp[]=new int[prices.length];
-        // int bp[]=new int[prices.length];
-        // sp[prices.length-1]=prices[prices.length-1];
-        // bp[0]=prices[0];
-        // for(int i=0;i<prices.length;i++){
-        //     if(i==0){
-        //         continue;
-        //     }
-        //     else{
-        //          bp[i]=Math.min(prices[i],bp[i-1]);
-        //     }
-        //     if(i==prices.length-1){
-        //         continue;
-        //     }
-        //     else{
-        //         sp[prices.length-i-1]=Math.max(prices[prices.length-i-1],sp[i+1]);
-        //     }           
-        
-        // int maxReturn=0;
-        // for(int i=0;i<prices.length;i++){
-        //     if(sp[i]>bp[i]){
-        //         maxReturn=Math.max(maxReturn,sp[i]-bp[i]);
-        //     }
-        // }
-        // return maxReturn;
-        
-        int bp=prices[0];
-        int maxReturn=0;
-        for(int i=1;i<prices.length;i++){
-            maxReturn=Math.max(prices[i]-bp,maxReturn);
-            bp=Math.min(prices[i],bp);
+        int[] sp=new int[prices.length];
+        int[] bp=new int[prices.length];
+        bp[0]=prices[0];
+        sp[prices.length-1]=prices[prices.length-1];
+        for(int i=0;i<prices.length;i++){
+            if(i!=0)
+                bp[i]=Math.min(bp[i-1],prices[i]);
+            if(i!=prices.length-1)
+                sp[i]=Math.max(sp[i+1],prices[i]);
         }
-        return maxReturn;
+        int maxProfit=0;
+        for(int i=0;i<prices.length;i++){
+            maxProfit=Math.max(maxProfit,sp[i]-bp[i]);
+        }
+        return maxProfit;
     }
 }
